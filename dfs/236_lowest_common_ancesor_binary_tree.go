b40ca32de1096cdc6402bc64b1354a2e9d7c5139
bing0n3 <root@bing0ne.com>
2019-09-05 15:34:22 -0400

    add 236 dfs

---
+++ dfs/236_lowest_common_ancesor_binary_tree.go
@@ -0,0 +1,34 @@
+package lowest_common_ancestor_binary_tree
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	// dfs
+	if root == nil {
+		return nil
+	}
+	if root == p || root == q {
+		return root
+	}
+
+	left := lowestCommonAncestor(root.Left, p, q)
+	right := lowestCommonAncestor(root.Right, p, q)
+
+	if left != nil && right != nil {
+		return root
+	}
+
+	if left == nil {
+		return right
+	}
+
+	if right == nil {
+		return left
+	}
+
+	return nil
+}
