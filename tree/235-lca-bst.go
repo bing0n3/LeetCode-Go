75eeac2c1dccb101816d7372b76b76788a242908
bing0n3 <ma@bing0ne.com>
2019-10-06 20:19:54 -0400

    235 binary search tree lca

---
+++ tree/235-lca-bst.go
@@ -0,0 +1,28 @@
+package tree
+
+func lowestCommonAncestor_1(root, p, q *TreeNode) *TreeNode {
+	for root != nil {
+		if root.Val > p.Val && root.Val > q.Val {
+			root = root.Left
+		} else if root.Val < p.Val && root.Val < q.Val {
+			root = root.Right
+		} else {
+			return root
+		}
+	}
+	return root
+}
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Val > p.Val && root.Val > q.Val {
+		return lowestCommonAncestor(root.Left, p, q)
+	}
+	if root.Val < p.Val && root.Val < q.Val {
+		return lowestCommonAncestor(root.Right, p, q)
+	}
+	return root
+
+}
